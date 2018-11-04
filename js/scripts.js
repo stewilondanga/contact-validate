16aa04c21b92be8a61705e78109860683fb54e59
Stewart ilondanga <stilondanga@gmail.com>
2018-11-04 12:52:31 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -26,105 +26,105 @@ $(document).ready(function() {
                   }
                 }
               },
-              /*            						last_name: {
-                          							 validators: {
-                          										stringLength: {
-                          											 min: 2,
-                          									 },
-                          									 notEmpty: {
-                          											 message: 'Please supply your last name'
-                          									 }
-                          							 }
-                          					 },
-                          					 email: {
-                          							 validators: {
-                          									 notEmpty: {
-                          											 message: 'Please supply your email address'
-                          									 },
-                          									 emailAddress: {
-                          											 message: 'Please supply a valid email address'
-                          									 }
-                          							 }
-                          					 },
-                          					 phone: {
-                          							 validators: {
-                          									 notEmpty: {
-                          											 message: 'Please supply your phone number'
-                          									 },
-                          									 phone: {
-                          											 country: 'US',
-                          											 message: 'Please supply a vaild phone number with area code'
-                          									 }
-                          							 }
-                          					 },
-                          					 address: {
-                          							 validators: {
-                          										stringLength: {
-                          											 min: 8,
-                          									 },
-                          									 notEmpty: {
-                          											 message: 'Please supply your street address'
-                          									 }
-                          							 }
-                          					 },
-                          					 city: {
-                          							 validators: {
-                          										stringLength: {
-                          											 min: 4,
-                          									 },
-                          									 notEmpty: {
-                          											 message: 'Please supply your city'
-                          									 }
-                          							 }
-                          					 },
-                          					 state: {
-                          							 validators: {
-                          									 notEmpty: {
-                          											 message: 'Please select your state'
-                          									 }
-                          							 }
-                          					 },
-                          					 zip: {
-                          							 validators: {
-                          									 notEmpty: {
-                          											 message: 'Please supply your zip code'
-                          									 },
-                          									 zipCode: {
-                          											 country: 'US',
-                          											 message: 'Please supply a vaild zip code'
-                          									 }
-                          							 }
-                          					 },
-                          					 comment: {
-                          							 validators: {
-                          										 stringLength: {
-                          											 min: 10,
-                          											 max: 200,
-                          											 message:'Please enter at least 10 characters and no more than 200'
-                          									 },
-                          									 notEmpty: {
-                          											 message: 'Please supply a description of your project'
-                          									 }
-                          									 }
-                          							 }
-                          					 }
-                          			 })
-                          			 .on('success.form.bv', function(e) {
-                          					 $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
-                          							 $('#contact_form').data('bootstrapValidator').resetForm();
+              last_name: {
+                validators: {
+                  stringLength: {
+                    min: 2,
+                  },
+                  notEmpty: {
+                    message: 'Please supply your last name'
+                  }
+                }
+              },
+              /*              					 email: {
+                            							 validators: {
+                            									 notEmpty: {
+                            											 message: 'Please supply your email address'
+                            									 },
+                            									 emailAddress: {
+                            											 message: 'Please supply a valid email address'
+                            									 }
+                            							 }
+                            					 },
+                            					 phone: {
+                            							 validators: {
+                            									 notEmpty: {
+                            											 message: 'Please supply your phone number'
+                            									 },
+                            									 phone: {
+                            											 country: 'US',
+                            											 message: 'Please supply a vaild phone number with area code'
+                            									 }
+                            							 }
+                            					 },
+                            					 address: {
+                            							 validators: {
+                            										stringLength: {
+                            											 min: 8,
+                            									 },
+                            									 notEmpty: {
+                            											 message: 'Please supply your street address'
+                            									 }
+                            							 }
+                            					 },
+                            					 city: {
+                            							 validators: {
+                            										stringLength: {
+                            											 min: 4,
+                            									 },
+                            									 notEmpty: {
+                            											 message: 'Please supply your city'
+                            									 }
+                            							 }
+                            					 },
+                            					 state: {
+                            							 validators: {
+                            									 notEmpty: {
+                            											 message: 'Please select your state'
+                            									 }
+                            							 }
+                            					 },
+                            					 zip: {
+                            							 validators: {
+                            									 notEmpty: {
+                            											 message: 'Please supply your zip code'
+                            									 },
+                            									 zipCode: {
+                            											 country: 'US',
+                            											 message: 'Please supply a vaild zip code'
+                            									 }
+                            							 }
+                            					 },
+                            					 comment: {
+                            							 validators: {
+                            										 stringLength: {
+                            											 min: 10,
+                            											 max: 200,
+                            											 message:'Please enter at least 10 characters and no more than 200'
+                            									 },
+                            									 notEmpty: {
+                            											 message: 'Please supply a description of your project'
+                            									 }
+                            									 }
+                            							 }
+                            					 }
+                            			 })
+                            			 .on('success.form.bv', function(e) {
+                            					 $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                            							 $('#contact_form').data('bootstrapValidator').resetForm();
 
-                          					 // Prevent form submission
-                          					 e.preventDefault();
+                            					 // Prevent form submission
+                            					 e.preventDefault();
 
-                          					 // Get the form instance
-                          					 var $form = $(e.target);
+                            					 // Get the form instance
+                            					 var $form = $(e.target);
 
-                          					 // Get the BootstrapValidator instance
-                          					 var bv = $form.data('bootstrapValidator');
+                            					 // Get the BootstrapValidator instance
+                            					 var bv = $form.data('bootstrapValidator');
 
-                          					 // Use Ajax to submit form data
-                          					 $.post($form.attr('action'), $form.serialize(), function(result) {
-                          							 console.log(result);
-                          					 }, 'json');
-                          			 });
-                          });
+                            					 // Use Ajax to submit form data
+                            					 $.post($form.attr('action'), $form.serialize(), function(result) {
+                            							 console.log(result);
+                            					 }, 'json');
+                            			 });
+                            });
