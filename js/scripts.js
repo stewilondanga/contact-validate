7d72dc0c85c61e6978a37220566498fcaaa72008
Stewart ilondanga <stilondanga@gmail.com>
2018-11-04 13:00:22 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -114,19 +114,19 @@ $(document).ready(function() {
               opacity: "show"
             }, "slow") // Do something ...
             $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
             /*
-                                    					 // Prevent form submission
-                                    					 e.preventDefault();
+                                                					 // Get the form instance
+                                                					 var $form = $(e.target);
 
-                                    					 // Get the form instance
-                                    					 var $form = $(e.target);
+                                                					 // Get the BootstrapValidator instance
+                                                					 var bv = $form.data('bootstrapValidator');
 
-                                    					 // Get the BootstrapValidator instance
-                                    					 var bv = $form.data('bootstrapValidator');
-
-                                    					 // Use Ajax to submit form data
-                                    					 $.post($form.attr('action'), $form.serialize(), function(result) {
-                                    							 console.log(result);
-                                    					 }, 'json');
-                                    			 });
-                                    });
+                                                					 // Use Ajax to submit form data
+                                                					 $.post($form.attr('action'), $form.serialize(), function(result) {
+                                                							 console.log(result);
+                                                					 }, 'json');
+                                                			 });
+                                                });
